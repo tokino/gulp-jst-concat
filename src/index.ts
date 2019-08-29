b8eb941ced80391b014c1bade620716327ec5ce2
tokino <t.kino0219@gmail.com>
2019-08-29 21:33:01 +0900

    fix gulp4, add typescript

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import through from "through2";
+import _ from 'lodash';
+import Vinyl from 'vinyl';
+import {PluginError} from 'gulp-util';
+
+const PLUGIN_NAME = 'gulp-jst-concat';
+
+function compile(file, renameKeys) {
+    const name = file.path.replace(new RegExp(renameKeys[0]), renameKeys[1]),
+        contents = String(file.contents);
+
+    return {
+        name: name,
+        fnSource: _.template(contents).source
+    }
+}
+
+function buildJSTString(file, renameKeys) {
+    const template = compile(file, renameKeys);
+    console.log(template.fnSource);
+    return `"${template.name}": ${template.fnSource}`;
+}
+
+module.exports = function jstConcat(fileName: string, _opts: any) {
+    if (!fileName) {
+        throw new PluginError(PLUGIN_NAME, 'Missing fileName')
+    }
+
+    const defaults = {renameKeys: ['.*', '$&']},
+        opts = _.extend({}, defaults, _opts),
+    files: string[] = [];
+
+    return through.obj(function (file, encoding, callback) {
+        if (file.isNull()) {
+            return callback(null, file);
+        }
+
+        if (file.isStream()) {
+            return this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+        }
+
+        const jstString = buildJSTString(file, opts.renameKeys);
+        files.push(jstString);
+        callback(null, file);
+    }, function (callback) {
+        this.push(new Vinyl({
+            path: fileName,
+            contents: Buffer.from(`this.JST = {${files.join(',\n')}};`)
+        }));
+        callback();
+    });
+
+};
